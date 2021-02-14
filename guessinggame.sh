0c4c1b873a99c375204e8d6b2cb8c50be70c045b
dgonzalez164 <diego.gonzalezc@udem.edu>
2021-02-14 11:35:53 -0600

    Initial commit for the guessinggame script

---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+number_of_files=$(ls | wc -l)
+echo "Go ahead, and guess the number of files in the current directory:"
+
+read user_input
+
+function checker {
+    while [ $user_input -ne $number_of_files ]
+    do
+    if [ $user_input -gt $number_of_files ]
+    then
+        echo "It’s higher than the actual value! Try again!"
+        read user_input
+    else
+        echo "It’s lower than the actual value! Try again!"
+        read user_input
+    fi
+    done
+}
+
+checker
+
+echo "Congratulations! You did it! You guessed the correct number of files: $number_of_files !"
